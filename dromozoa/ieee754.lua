3fd52d797ca440db4549edb2a51fd7719211287e
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-04-10 22:32:00 +0900

    ieee754を追加。

---
+++ dromozoa/ieee754.lua
@@ -0,0 +1,168 @@
+if string.pack then
+  local function format(size, endian)
+    if not endian then
+      endian = "="
+    end
+    if not size then
+      return endian .. "n"
+    elseif size == 4 then
+      return endian .. "f"
+    elseif size == 8 then
+      return endian .. "d"
+    end
+  end
+
+  return {
+    decode = function (s, endian)
+      return (format(#s, endian):unpack(s))
+    end;
+
+    encode = function (v, size, endian)
+      return format(size, endian):pack(v)
+    end;
+  }
+else
+  local unpack = table.unpack or unpack
+
+  local native_endian
+  local native_size
+  do
+    local chunk = string.dump(function () end)
+    if chunk:sub(1, 4) == "\27Lua" then
+      local version = chunk:byte(5)
+      if version == 81 or version == 82 then
+        if chunk:byte(7) == 1 then
+          native_endian = "<"
+        else
+          native_endian = ">"
+        end
+        native_size = chunk:byte(11)
+      end
+    end
+  end
+
+  local function format(size, endian)
+    if not size then
+      size = native_size
+    end
+    if not endian then
+      endian = native_endian
+    end
+    return size, endian
+  end
+
+  local function constant(size)
+    if size == 4 then
+      return 126, 255, 128
+    elseif size == 8 then
+      return 1022, 2047, 16
+    end
+  end
+
+  local function swap(buffer)
+    local n = #buffer
+    for i = 1, n / 2 do
+      local j = n - i + 1
+      buffer[i], buffer[j] = buffer[j], buffer[i]
+    end
+  end
+
+  local function encode_fraction(v, shift)
+    local a = v * shift
+    local b = math.floor(a)
+    return a - b, b
+  end
+
+  return {
+    decode = function (s, endian)
+      local size, endian = format(#s, endian)
+      local bias, fill, shift = constant(size)
+
+      local buffer = { s:byte(1, -1) }
+      if endian == "<" then
+        swap(buffer)
+      end
+
+      local a, b = buffer[1], buffer[2]
+      local sign = a < 128 and 1 or -1
+      local exponent = a % 128 * shift + math.floor(b / shift)
+      local fraction = 0
+      for i = #buffer, 3, -1 do
+        fraction = (fraction + buffer[i]) / 256
+      end
+      fraction = (fraction + b % shift) / shift
+
+      if exponent == fill then
+        if fraction == 0 then
+          return sign * math.huge
+        else
+          return 0 / 0
+        end
+      elseif exponent == 0 then
+        if fraction == 0 then
+          if sign > 0 then
+            return 0
+          else
+            return -1 / math.huge
+          end
+        else
+          return sign * math.ldexp(fraction, exponent - bias)
+        end
+      else
+        return sign * math.ldexp((fraction + 1) / 2, exponent - bias)
+      end
+    end;
+
+    encode = function (v, size, endian)
+      local size, endian = format(size, endian)
+      local bias, fill, shift = constant(size)
+
+      local sign = 0
+      local exponent = 0
+      local fraction = 0
+
+      if -math.huge < v and v < math.huge then
+        if v == 0 then
+          if string.format("%g", v):sub(1, 1) == "-" then
+            sign = 0x80
+          end
+        else
+          if v < 0 then
+            sign = 0x80
+          end
+          local m, e = math.frexp(v)
+          if e <= -bias then
+            fraction = math.ldexp(m, e + bias)
+          else
+            exponent = e + bias
+            fraction = m * 2 - 1
+          end
+        end
+      else
+        exponent = fill
+        if v ~= math.huge then
+          sign = 0x80
+          if v ~= -math.huge then
+            fraction = 0.5
+          end
+        end
+      end
+
+      local buffer = {}
+      fraction, buffer[2] = encode_fraction(fraction, shift)
+      for i = 3, size do
+        fraction, buffer[i] = encode_fraction(fraction, 256)
+      end
+
+      local a = exponent * shift
+      local b = a % 256
+      buffer[1] = sign + (a - b) / 256
+      buffer[2] = buffer[2] + b
+
+      if endian == "<" then
+        swap(buffer)
+      end
+      return string.char(unpack(buffer))
+    end;
+  }
+end
--- number.lua
+++ number.lua
@@ -17,7 +17,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-assembler.  If not, see <http://www.gnu.org/licenses/>.
 
-local number = require "dromozoa.number"
+local ieee754 = require "dromozoa.ieee754"
 
 local DBL_MAX = 1.7976931348623157e+308
 local DBL_DENORM_MIN = 4.9406564584124654e-324
@@ -25,168 +25,29 @@ local DBL_MIN = 2.2250738585072014e-308
 local DBL_EPSILON = 2.2204460492503131e-16
 local nan = math.sqrt(-1)
 
--- io.write(number.encode_binary(0.125, 8, "<"))
-print(number.decode_binary(number.encode_binary(DBL_MIN, 8, "<"), "<"))
--- io.write(number.encode_binary(0.375, 4, ">"))
-os.exit()
+local function is_nan(v)
+  return not (-math.huge <= v and v <= math.huge)
+end
 
-local unpack = table.unpack or unpack
-
-local encode_binary
-local decode_binary
-
-if string.pack then
-  encode_binary = function (v)
-    return string.pack("<d", v)
-  end
-
-  decode_binary = function (v)
-    return string.unpack("<d", v)
-  end
-else
-  encode_binary = function (v)
-    local sign = 0
-    local exponent = 0
-    local fraction = 0
-
-    if -math.huge < v and v < math.huge then
-      if v == 0 then
-        if string.format("%.17g", v):sub(1, 1) == "-" then
-          sign = 1
-        end
-      else
-        if v < 0 then
-          sign = 1
-        end
-        local m, e = math.frexp(v)
-        if e < -1021 then
-          fraction = math.ldexp(m, e + 1022)
-        else
-          exponent = e + 1022
-          fraction = m * 2 - 1
-        end
-      end
-    elseif v == math.huge then
-      exponent = 2047
-    elseif v == -math.huge then
-      sign = 1
-      exponent = 2047
-    else
-      sign = 1
-      exponent = 2047
-      fraction = 0.5
-    end
-
-    io.stderr:write(string.format("E %d,%d,%.17g\n", sign, exponent, fraction))
-
-    local buffer = {}
-
-    local a = exponent % 16
-    buffer[1] = sign * 128 + (exponent - a) / 16
-
-    local f = fraction * 16
-    local b = math.floor(f)
-    f = f - b
-    buffer[2] = a * 16 + b
-
-    for i = 3, 8 do
-      f = f * 256
-      b = math.floor(f)
-      f = f - b
-      buffer[i] = b
-    end
-
-    for i = 1, 4 do
-      buffer[i], buffer[9 - i] = buffer[9 - i], buffer[i]
-    end
-
-    return string.char(unpack(buffer))
-  end
-
-  decode_binary = function (v)
-    local buffer = { v:byte(1, -1) }
-    for i = 1, 4 do
-      buffer[i], buffer[9 - i] = buffer[9 - i], buffer[i]
-    end
-
-    local sign = 0
-    local exponent = 0
-    local fraction = 0
-
-    local a = buffer[1]
-    local b = a % 128
-
-    if a - b == 128 then
-      sign = 1
-    end
-    exponent = b * 16
-
-    local a = buffer[2]
-    local b = a % 16
-    exponent = exponent + (a - b) / 16
-    fraction = b
-
-    local f = 0
-    for i = 8, 3, -1 do
-      f = f / 256
-      f = f + buffer[i]
-    end
-    f = f / 256
-
-    fraction = (fraction + f) / 16
-
-    if exponent == 2047 then
-      if fraction == 0 then
-        if sign == 0 then
-          io.stderr:write("= INF\n")
-          return math.huge
-        else
-          io.stderr:write("= -INF\n")
-          return -math.huge
-        end
-      else
-        io.stderr:write("= NaN\n")
-        return 0 / 0
-      end
-    elseif exponent == 0 and fraction == 0 then
-      if sign == 0 then
-        io.stderr:write("= 0\n")
-        return 0
-      else
-        io.stderr:write("= -0\n")
-        return -1 / math.huge
-      end
-    else
-      local m, e
-      if exponent == 0 then
-        m = fraction
-        e = exponent - 1022
-      else
-        m = (fraction + 1) / 2
-        e = exponent - 1022
-      end
-      io.stderr:write(string.format("= %.17g\n", math.ldexp(m, e)))
-      return math.ldexp(m, e)
-    end
-
-    io.stderr:write(string.format("D %d,%d,%.17g\n", sign, exponent, fraction))
+local function test(u)
+  local s = ieee754.encode(u, 8, "<")
+  -- io.write(s)
+  local v = ieee754.decode(s, "<")
+  print("<", u)
+  print(">", v)
+  if is_nan(u) then
+    assert(is_nan(v))
+  else
+    assert(u == v)
   end
 end
 
-local sign_zero = -DBL_MIN / 256^7
-
-local function test(v)
-  io.stderr:write(string.format("? %.17g\n", v))
-  print(decode_binary(encode_binary(v)))
-  io.stderr:write("\n")
-end
-
-test(sign_zero)
+test(-1 / math.huge)
 test(0)
 test(DBL_MAX)
 test(DBL_MIN)
-test(2^-1030)
 test(math.pi)
+test(2^-1030)
 test(math.huge)
 test(-math.huge)
 test(nan)
